7aabe0884977a450fa7212de5d9a5abe563906f6
vitor <vgalmeida10@gmail.com>
2025-05-17 12:02:33 -0300

    corrigindo

--- back-end/app/main.py
+++ back-end/app/main.py
@@ -21,8 +21,9 @@ app.add_middleware(
     allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
-    allow_headers=["*"],
+    allow_headers=["*"]
 )
+
 
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(user_router, prefix="/users", tags=["users"])
